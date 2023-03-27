53b96c890426d62146f5f894e1f649dae2639d96
Stephan Syntax <stephan.mvb@syntax-institut.de>
2023-03-27 09:52:41 +0200

    Aufgabe 4 angepasst

--- Aufgabe4/src/4_Textabgabe.kt
+++ Aufgabe4/src/4_Textabgabe.kt
@@ -1,17 +1,4 @@
 /*
-fun animal() {
-    val myAnimal: String = "Dog"
-    print(myAnimal)
-}
-fun number() {
-    val myNumber: Int = 8
-    print(myNumber)
-}
-fun main(){
-    number()
-    animal()
-}
-
-Was ist die Ausgabe von diesem Codeabschnitt?
+Was ist die Ausgabe vom Codeabschnitt?
 
  */
