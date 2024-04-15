aac4caf9eee0d00531afc797c043a375b0587d35
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-04-15 07:19:42 +0100

    quiz

---
+++ Aufgabe00/src/Aufgabe00.kt
@@ -0,0 +1,37 @@
+/*
+Frage 1
+Was ist das Hauptziel einer Funktion in der Programmierung?
+A) Daten in einer Datenbank zu speichern.
+B) Eine spezifische Aufgabe zu erfüllen und den Code zu strukturieren.
+C) Die Geschwindigkeit des Programms zu erhöhen.
+D) Grafische Benutzeroberflächen zu erstellen.
+
+Frage 2
+Was kennzeichnet die `main()`-Funktion in einem Kotlin-Programm?
+A) Sie ist immer die letzte Funktion, die ausgeführt wird.
+B) Sie dient als Steuerzentrum und Einstiegspunkt des Programms.
+C) Sie wird verwendet, um Datenbankverbindungen zu verwalten.
+D) Sie kann nur einfache Ausdrücke ausführen.
+
+Frage 3
+Wie rufst du eine benutzerdefinierte Funktion namens `sayHello()` in Kotlin auf?
+A) `sayHello().run()`
+B) `call sayHello()`
+C) `sayHello()`
+D) `execute sayHello()`
+
+Frage 4
+Wie würde eine Funktion in Kotlin behandelt, wenn sie in der main()-Funktion nicht aufgerufen wird?
+A) Sie wird automatisch gelöscht.
+B) Sie wird trotzdem ausgeführt.
+C) Ihr Name bleibt in der IDE ausgegraut, was darauf hinweist, dass sie unbenutzt ist.
+D) Sie verursacht einen Fehler beim Kompilieren.
+
+Frage 5
+Welches Prinzip empfiehlt, dass Funktionen nur eine klare Aufgabe erfüllen sollen?
+A) KISS-Prinzip (Keep It Simple, Stupid)
+B) Single Responsibility Principle (SRP) 
+C) Don't Repeat Yourself (DRY)
+D) Divide and Conquer
+
+ */
